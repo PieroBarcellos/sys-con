3c15dcac8bc579dac233a6b839df52f3791e2812
PieroBarcellos <119296428+PieroBarcellos@users.noreply.github.com>
2022-12-20 23:24:01 -0300

    Add files via upload

--- source/ControllerLib/Controllers.h
+++ source/ControllerLib/Controllers.h
@@ -4,6 +4,6 @@
 #include "Controllers/Xbox360WirelessController.h"
 #include "Controllers/XboxController.h"
 #include "Controllers/XboxOneController.h"
+#include "Controllers/SteeringWheelSDKSimple.h"
 #include "Controllers/Dualshock3Controller.h"
 #include "Controllers/Dualshock4Controller.h"
-#include "Controllers/WheelSuports.h"
